3dae6401dec766d80f401b9ee86db0eb285b7b54
Jose RF Junior <web2ajax@gmail.com>
2020-07-12 13:57:50 -0400

    Update app.component.ts

--- grpc-demo-aspnetcore31/src/app/app.component.ts
+++ grpc-demo-aspnetcore31/src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import {grpc} from "@improbable-eng/grpc-web";
-import {CountryService} from "./generated/country_pb_service";
-import {EmptyRequest, CountriesReply} from "./generated/country_pb";
+import { grpc } from '@improbable-eng/grpc-web';
+import {CountryService, CountryServiceCreate, CountryServiceDelete,
+  CountryServiceGetAll, CountryServiceGetById, CountryServiceUpdate } from './generated/country_pb_service';
+import {EmptyRequest, CountriesReply, CountryCreateRequest, CountrySearchRequest } from './generated/country_pb';
 import { CountryModel } from './models/countryModel';
+import {host} from '@angular-devkit/build-angular/src/test-utils';
+
 
 @Component({
   selector: 'app-root',
@@ -11,27 +14,38 @@ import { CountryModel } from './models/countryModel';
 })
 export class AppComponent implements OnInit {
 
-  public title = 'grpc-web-demo';
-  public countries: CountryModel[] = []; 
-
+  public title = 'gRPC';
+  public countries: CountryModel[] = [];
+  public host  = 'https://demogrpcweblinux.azurewebsites.net'; /* host: 'https://demogrpcweblinux.azurewebsites.net',*/
   public ngOnInit() {
 
-    const getCountryRequest = new EmptyRequest();
+    const getCountryServiceGetById = new EmptyRequest();
+    const getCountryServiceCreate  = new EmptyRequest();
+    const getCountryServiceUpdate  = new EmptyRequest();
+    const getCountryServiceDelete  = new EmptyRequest();
 
+    this.getCoutryFindAll();
+  }
+
+  getCoutryFindAll() {
+    const getCountryServiceGetAll  = new EmptyRequest();
     grpc.unary(CountryService.GetAll, {
-      request: getCountryRequest,
-      host: "https://demogrpcweblinux.azurewebsites.net", //https://grpcwebdemo.azurewebsites.net (Windows App Service)
+      request: getCountryServiceGetAll,
+      host: this.host,
       onEnd: res => {
-        const { status, statusMessage, headers, message, trailers } = res;
+        const { status, message} = res;
         if (status === grpc.Code.OK && message) {
-        var result = message.toObject() as CountriesReply.AsObject;
-        this.countries = result.countriesList.map(country => 
-          <CountryModel>({
-            name: country.name,
-            description: country.description
-          }));
+          const result = message.toObject() as CountriesReply.AsObject;
+          this.countries = result.countriesList.map(country =>
+            ({
+              id: country.id,
+              name: country.name,
+              description: country.description
+            }) as CountryModel);
         }
       }
     });
   }
+
 }
+
